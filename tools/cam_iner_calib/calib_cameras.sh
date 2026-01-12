3c2365c9059b04aca0e05ddea96126b58e5a7529
sssxxxkkk <843401422@qq.com>
2026-01-12 17:29:38 +0800

    add calib functions

---
+++ tools/cam_iner_calib/calib_cameras.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source ../../../kalibr2/devel/setup.bash
+
+
+rosrun kalibr kalibr_calibrate_cameras --target aprilgrid.yaml --bag ../../save_data/output.bag --models pinhole-radtan --topics /camera/image_raw --show-extraction
